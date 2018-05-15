334104a7ecb39ed2d4c8a87c02807c584aee1e80
Mark Williams <mark@n3twork.com>
2018-05-14 20:17:34 -0700

    Correctly call super default constructor for test class

--- src/test/java/com/n3twork/dynamap/TestDocumentBeanSubclass.java
+++ src/test/java/com/n3twork/dynamap/TestDocumentBeanSubclass.java
@@ -5,6 +5,7 @@ import com.n3twork.dynamap.test.TestDocumentBean;
 public class TestDocumentBeanSubclass extends TestDocumentBean {
 
     protected TestDocumentBeanSubclass() {
+        super();
     }
 
     public TestDocumentBeanSubclass(String id, Integer sequence) {
